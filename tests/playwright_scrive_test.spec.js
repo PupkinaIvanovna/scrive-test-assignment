9c60cd8f893b35069a6ffe702e72c312c2410a8e
Kate Savitskaya <e.i.savitskaya@gmail.com>
2024-10-06 23:42:37 +0200

    updated test

--- tests/playwright_scrive_test.spec.js
+++ tests/playwright_scrive_test.spec.js
@@ -1,28 +1,19 @@
-const { webkit, chromium, firefox } = require("playwright");
+import { test, expect } from "@playwright/test";
 
-(async () => {
-  for (const browserType of [webkit, firefox, chromium]) {
-    const browser = await browserType.launch({
-      headless: false,
-    });
-    const page = await browser.newPage();
-
-    await page.goto(
-      "https://staging.scrive.com/t/9221714692410699950/7348c782641060a9"
-    );
-    await page.locator(".info-text-input").click();
-    await page.getByPlaceholder("Your name").fill("Kate Savitskaya");
-    //await expect(page.getByRole("button", { name: "Next" })).toBeVisible();
-    await page.getByRole("button", { name: "Next" }).click();
-    //await expect(page.locator("div:nth-child(5)").first()).toBeVisible();
-    await page.waitForTimeout(3000);
-    await page
-      .locator("div:nth-child(5)")
-      .first()
-      .screenshot({ path: "screenshot.png" });
-    await page.getByRole("button", { name: "Sign" }).click();
-    // await expect(page.getByText("Document signed!")).toBeVisible();
-    console.log("Success");
-    await browser.close();
-  }
-})();
+test("test", async ({ page }) => {
+  await page.goto(
+    "https://staging.scrive.com/t/9221714692410699950/7348c782641060a9"
+  );
+  await page.locator(".info-text-input").click();
+  await page.getByPlaceholder("Your name").fill("Kate Savitskaya");
+  await expect(page.getByRole("button", { name: "Next" })).toBeVisible();
+  await page.getByRole("button", { name: "Next" }).click();
+  await expect(page.locator("div:nth-child(5)").first()).toBeVisible();
+  await page.waitForTimeout(3000);
+  await page
+    .locator("div:nth-child(5)")
+    .first()
+    .screenshot({ path: "screenshot.png" });
+  await page.getByRole("button", { name: "Sign" }).click();
+  await expect(page.getByText("Document signed!")).toBeVisible();
+});
